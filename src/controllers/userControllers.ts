7d368a343665d5697d7e982f9e45ca7ac22b8e27
egoloprum <bayraa093@gmail.com>
2025-12-29 13:33:22 +0300

    feature: get list of followings api

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,12 +1,12 @@
 import type { Context } from 'hono'
 import { getSupabaseClient } from '../lib/supabase'
-import type { AuthContext, User } from '../lib/types'
+import type { User } from '../lib/types'
 import type { PostgrestError } from '@supabase/supabase-js'
 import { responseError } from '../lib/helpers'
 
 type PublicUser = Pick<User, 'id' | 'username' | 'avatar_url'>
 
-export const getFollowersOfUser = async (c: Context<AuthContext>) => {
+export const getFollowersOfUser = async (c: Context) => {
   const { supabaseAnon } = getSupabaseClient(c)
 
   const user_id = c.req.param('user_id')
@@ -18,7 +18,6 @@ export const getFollowersOfUser = async (c: Context<AuthContext>) => {
     .returns<PublicUser[]>()) as { data: PublicUser[] | null; error: PostgrestError | null }
 
   if (error) {
-    console.error('Error fetching followers:', error)
     return responseError(
       {
         code: 'POSTGRES_ERROR',
@@ -33,7 +32,28 @@ export const getFollowersOfUser = async (c: Context<AuthContext>) => {
 }
 
 export const getFollowingOfUser = async (c: Context) => {
-  return c.json({ message: 'hello world' }, 200)
+  const { supabaseAnon } = getSupabaseClient(c)
+
+  const user_id = c.req.param('user_id')
+
+  const { data: followings, error } = (await supabaseAnon
+    .rpc('get_user_following', {
+      target_user_id: user_id,
+    })
+    .returns<PublicUser[]>()) as { data: PublicUser[] | null; error: PostgrestError | null }
+
+  if (error) {
+    return responseError(
+      {
+        code: 'POSTGRES_ERROR',
+        message: error.message,
+      },
+      'Failed to fetch followings',
+      { status: 500 }
+    )
+  }
+
+  return c.json({ followings }, 200)
 }
 
 export const getFollowStatus = async (c: Context) => {
